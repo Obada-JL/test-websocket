60c11ed44f14aed2f7f49f235cf3de727f8b29e4
Obada-JL <leader.obada@gmail.com>
2024-05-31 16:55:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { join } = require("path");
+const app = express();
+const cors = require("cors");
+const { Server } = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+app.use(cors());
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+io.on("connection", (socket) => {
+  console.log("a user connected with socket id ", socket.id);
+  socket.on("chat message", (msg) => {
+    io.emit("send_message_to_all_users", msg);
+  });
+  socket.on("typing", () => {
+    socket.broadcast.emit("show_typing_status");
+  });
+  socket.on("stop_typing", () => {
+    socket.broadcast.emit("clear_typing_status");
+  });
+  socket.on("disconnect", () => {
+    console.log("left the chat with socket id " + socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on port 3000");
+});
